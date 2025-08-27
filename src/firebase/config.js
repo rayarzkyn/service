4b151835ed540cb6a3ad2c8ac2d155330aca765b
Raya Rizkyana <rayarzkyn23@gmail.com>
2025-08-27 11:56:22 +0700

    fix: perbaiki firebase config dan env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -10,8 +10,15 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
+// Cek kalau API key kosong
+if (!firebaseConfig.apiKey) {
+  throw new Error(
+    "Firebase API Key is missing. Pastikan environment variables sudah diatur di Vercel dan .env.local."
+  );
+}
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
